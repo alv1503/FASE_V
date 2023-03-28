948610bd94750e0ccedf1527f815dd0546f1b2eb
alv1503 <alvaroarbona2003@gmail.com>
2023-03-28 19:22:05 +0200

    Initial commit

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/alvaro/Documents/GitHub/FASE_V/devel:$CMAKE_PREFIX_PATH"
+export PWD='/home/alvaro/Documents/GitHub/FASE_V/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/alvaro/Documents/GitHub/FASE_V/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/alvaro/Documents/GitHub/FASE_V/src:$ROS_PACKAGE_PATH"
